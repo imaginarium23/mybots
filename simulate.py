78905e9b3f7cd947ca7bf8824fd9adc2ff8c930c
imaginarium23 <sarahnaghmi@gmail.com>
2023-06-23 17:24:24 -0400

    adding gravity and floor

--- simulate.py
+++ simulate.py
@@ -1,8 +1,16 @@
 import pybullet as p
 import time
-
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0,0,-9.8) #adds gravity
+
+planeId = p.loadURDF("plane.urdf") #adds floor
+
+p.loadSDF("box.sdf") #adds box
 
 for i in range(1000):
     p.stepSimulation()
