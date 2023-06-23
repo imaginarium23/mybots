e32942a8b001b10830fc7b5145be060647fb6583
imaginarium23 <sarahnaghmi@gmail.com>
2023-06-23 16:46:45 -0400

    adding pyrosim

--- simulate.py
+++ simulate.py
@@ -1,1 +1,13 @@
-pass
+import pybullet as p
+import time
+
+
+physicsClient = p.connect(p.GUI)
+
+for i in range(1000):
+    p.stepSimulation()
+    print(i) # Print the value of the loop variable
+    time.sleep(1/60) # Sleep for 1/60th of a second
+
+
+p.disconnect()
